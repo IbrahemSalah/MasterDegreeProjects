f98bf4eb78d447cd1afbd96bd00a857b78d705fe
Ibrahem Salah <>
2023-04-04 15:11:23 -0500

    Second Assignment

---
+++ Ibrahem Salah 615885 Assignment 2/RandomBackgroundApp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding as ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+
+    private fun initViews() {
+
+        binding.btnSetBackground.setOnClickListener {
+           binding.root.setBackgroundColor(getRandomColor())
+            binding.ibMiuLogo.setBackgroundColor(getRandomColor())
+        }
+    }
+
+    private fun getRandomColor(): Int {
+        val rnd = Random()
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+    }
+}
