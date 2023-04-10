1cbfd29c8ef4cb3ccda0688027edffdb1008ed7d
ahmed <agendyyy@gmail.com>
2023-04-10 03:59:30 -0500

    test commit

--- Group Project Lab6 Problem 1/QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ Group Project Lab6 Problem 1/QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //asdads
+        //asdadsasdasd
         submitButtonHandler()
         resetBtnHandler()
     }
