ccffd0994304b522c53421b7eed7884b8cb6559e
Osama Alawneh <osama@Osamas-MacBook-Pro.local>
2023-04-10 00:46:22 -0500

    Adding qizu app into repo

---
+++ Group Project Lab6 Problem 1/QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.quizapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
