e736a1b1096661b18a356769bd56d269853f07f8
Ibrahem Salah <>
2023-04-17 02:57:41 -0500

    Adding bottom navigation code.

--- Assignment 6/app/src/main/java/com/android/assignment6/HomeActivity.kt
+++ Assignment 6/app/src/main/java/com/android/assignment6/HomeActivity.kt
@@ -1,11 +1,31 @@
 package com.android.assignment6
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.android.assignment6.databinding.ActivityHomeBinding
+import com.android.assignment6.ui.base.BaseActivity
 
-class HomeActivity : AppCompatActivity() {
+
+class HomeActivity : BaseActivity() {
+    private var _binding: ActivityHomeBinding? = null
+    private val binding: ActivityHomeBinding get() = _binding as ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        _binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
     }
+
+    private fun initViews() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.dashboard_navigation_graph_host) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
 }
