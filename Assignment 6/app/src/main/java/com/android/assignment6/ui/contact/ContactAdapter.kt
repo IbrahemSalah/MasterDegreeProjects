e6070b50796ee28321a640020a2b94e5b1fad9d3
Taha Elsayed <tahazayed@gmail.com>
2023-04-17 02:07:56 -0500

    Add Contacts

---
+++ Assignment 6/app/src/main/java/com/android/assignment6/ui/contact/ContactAdapter.kt
@@ -0,0 +1,48 @@
+package com.android.assignment6.ui.contact
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.android.assignment6.R
+
+class ContactAdapter(var contacts: ArrayList<Contact>) :
+    RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ContactAdapter.ContactViewHolder {
+        // parent --> RecyclerView UI, is a ViewGroup
+        // false, already we attached to parent.context, simply false
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.contact_item_list, parent, false)
+        return ContactViewHolder(view)
+    }
+    /* Sets the contents of an item at a given position in the RecyclerView.
+    Called by RecyclerView to display the data at a specified position over and over.
+   */
+
+    override fun onBindViewHolder(holder: ContactAdapter.ContactViewHolder, position: Int) {
+        holder.link.text = contacts[position].link
+        holder.description.text = contacts[position].description
+        holder.icon.setImageResource(contacts[position].imageId)
+        if (position == 0) {
+            holder.link.setBackgroundColor(Color.DKGRAY)
+            holder.description.setBackgroundColor(Color.TRANSPARENT)
+        }
+    }
+
+    override fun getItemCount() = contacts.size
+
+    /*  override fun getItemCount(): Int {
+          return books.size
+      }*/
+    inner class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var link: TextView = itemView.findViewById(R.id.tv_link)
+        var description: TextView = itemView.findViewById(R.id.tv_description)
+        var icon: ImageView = itemView.findViewById(R.id.iv_icon)
+    }
+}
--- Assignment 6/app/src/main/java/com/android/assignment6/ui/contact/ContactFragment.kt
+++ Assignment 6/app/src/main/java/com/android/assignment6/ui/contact/ContactFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.android.assignment6.R
 import com.android.assignment6.databinding.FragmentContactBinding
 import com.android.assignment6.ui.base.BaseViewModelFragment
 import com.android.assignment6.ui.home.HomeViewModel
@@ -18,6 +20,24 @@ class ContactFragment : BaseViewModelFragment<HomeViewModel, FragmentContactBind
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentContactBinding.inflate(layoutInflater)
+
+        val contacts = ArrayList<Contact>()
+        contacts.add(Contact("+1(641)233-9470", "Phone", R.drawable.phone))
+        contacts.add(Contact("developer@gmail.com", "Email", R.drawable.envelope))
+        contacts.add(
+            Contact(
+                "https://www.linkedin.com/in/tahaamin/",
+                "LinkedIn Website",
+                R.drawable.linkedin
+            )
+        )
+        contacts.add(Contact("https://github.com/tahazayed", "GitHub", R.drawable.github))
+        contacts.add(Contact("PDF", "Resume PDF", R.drawable.pdf))
+
+        var ad_ob = ContactAdapter(contacts)
+        binding.rcv.layoutManager = GridLayoutManager(getActivity(),2)
+        // binding.rcv.layoutManager = LinearLayoutManager(this)
+        binding.rcv.adapter = ad_ob
         return binding.root
     }
 
